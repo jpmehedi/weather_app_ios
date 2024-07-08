d190afd6b27cfc373f30c6f65b3b0466baba4cc3
cmatrixgithub <cmatrixgithub@gmail.com>
2024-07-08 12:23:51 +0900

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Corporation CMatrix on 2024/07/08.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
